2582be1934ace5dc4c62cef50a605b4216a42049
kihwan <kihwan_.choi@samsung.com>
2019-11-22 19:57:13 +0900

    Adjust DI(koin) & modify button style

--- app/src/main/java/com/redbeanlatte11/factchecker/FactCheckerApplication.kt
+++ app/src/main/java/com/redbeanlatte11/factchecker/FactCheckerApplication.kt
@@ -4,28 +4,29 @@ import android.app.Application
 import com.redbeanlatte11.factchecker.data.source.ChannelsRepository
 import com.redbeanlatte11.factchecker.data.source.VideosRepository
 import net.danlew.android.joda.JodaTimeAndroid
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
 import timber.log.Timber
 
 class FactCheckerApplication : Application() {
 
-    val videoRepository: VideosRepository
-        get() = ServiceLocator.provideVideosRepository(this)
+    val videoRepository: VideosRepository by inject(named("blacklist"))
 
-    val poplarVideoRepository: VideosRepository
-        get() = ServiceLocator.providePopularVideosRepository(this)
+    val poplarVideoRepository: VideosRepository by inject(named("popular"))
 
-    val channelRepository: ChannelsRepository
-        get() = ServiceLocator.provideChannelsRepository(this)
+    val channelRepository: ChannelsRepository by inject()
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         JodaTimeAndroid.init(this)
         startKoin {
+            androidLogger()
             androidContext(this@FactCheckerApplication)
-            modules(appModule)
+            modules(listOf(appModule))
         }
     }
 }
